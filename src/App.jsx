181d2152aeec2eff35b14c6154e280951c59de9c
thepassant <bassant.ayman69@gmail.com>
2024-02-15 18:19:55 +0200

    add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import CertificateRequestForm from "./components/CertificateRequestForm";
+import RequestsList from "./components/RequestsList";
 
 function App() {
   return (
     <>
-      <div>
-        <CertificateRequestForm />
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <Link to="/create-request">
+              <button>Create Certificate Request</button>
+            </Link>
+            <Link to="/view-requests">
+              <button>View Submitted Requests</button>
+            </Link>
+          </nav>
+
+          <Routes>
+            <Route
+              path="/create-request"
+              element={<CertificateRequestForm />}
+            />
+            <Route path="/view-requests" element={<RequestsList />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
